3436f0e955262619c5f079ec434a6b1087d2f0b3
zhangshen <1241325747@qq.com>
2019-12-31 14:34:40 +0800

    encounter a null pointer (because of array copy) bug

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -5,10 +5,28 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.util.Vector;
 
 public class MainFrame extends JFrame {
     // 内容窗格
     private Container mContainer;
+
+    // 需要进行访问控制的根目录
+    private File mRootDir;
+    // 根目录下的所有文件
+    private String[] mFileList;
+
+    // 可以进行“读取”和“写入”两种操作
+    private String[] mOptList = new String[]{"读取", "写入"};
+
+    // 选择目录文本标签
+    private JTextField mTextField = new JTextField(25);
+
+    // 文件列表组件
+    private JList waitChooseFileList;
+    // 操作列表组件
+    private JList waitChooseOptList;
 
 
     // constructor
@@ -25,103 +43,130 @@ public class MainFrame extends JFrame {
 
     /**
      * 得到一个选项卡面板
+     *
      * @return 一个选项卡面板
      */
     private JTabbedPane getTabbedPane() {
 
         JTabbedPane tabbedPane = new JTabbedPane();
 
-        JComponent p1 = getFilePanel();
+        JPanel p1 = getFilePanel();
         tabbedPane.addTab("文件面板", p1);
-        tabbedPane.setMnemonicAt(0,KeyEvent.VK_1);
+//        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
-        JComponent p2 = getFilePanel();
-        tabbedPane.addTab("文件面板", p2);
-        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-
-        JComponent p3 = getUserPanel();
-        tabbedPane.addTab("演示面板", p3);
-        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+        JPanel p2 = getUserPanel();
+        tabbedPane.addTab("用户面板", p2);
+//        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
 
         return tabbedPane;
     }
 
 
-    private JPanel getFileSelectPanel(){
-        JPanel panel = new JPanel(new FlowLayout());
-        // TODO
-        return panel;
-    }
-
-    /**
-     * 得到一个文件选择组件
-     * @return  一个自定义文件选择组件
-     */
-    private JPanel getMyFileChooser() {
-        JPanel myFileChooser = new JPanel();
-
-        JLabel label = new JLabel("所选目录路径：");
-        JTextField jText = new JTextField(25);
-        JButton button = new JButton("浏览");
-
-        myFileChooser.add(label);
-        myFileChooser.add(jText);
-        myFileChooser.add(button);
-
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                JFileChooser fc = new JFileChooser();
-                fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int val = fc.showOpenDialog(null);    //文件打开对话框
-                if (val == fc.APPROVE_OPTION) {
-                    //正常选择文件
-                    jText.setText(fc.getSelectedFile().toString());
-
-
-                } else {
-                    //未正常选择文件，如选择取消按钮
-                    jText.setText("未选择文件");
-                }
-            }
-        });
-        return myFileChooser;
-    }
-
     /**
      * 配置并返回文件面板
+     *
      * @return
      */
     private JPanel getFilePanel() {
-        JPanel filePanel = new JPanel(new GridLayout(2, 1));
-        filePanel.add(getMyFileChooser());
+        JPanel filePanel = new JPanel();
+
+        JLabel label = new JLabel("所选目录路径：");
+        JButton button = new JButton("浏览");
+
+        filePanel.add(label);
+        filePanel.add(mTextField);
+        filePanel.add(button);
+
+
+        waitChooseFileList = new JList(mFileList);
+        waitChooseFileList.setBorder(BorderFactory.createTitledBorder("请选择要操作的文件："));
+        waitChooseFileList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        if (mOptList != null) {
+            mOptList = new String[]{"读取", "写入"};
+        }
+        waitChooseOptList = new JList(mOptList);
+        waitChooseOptList.setBorder(BorderFactory.createTitledBorder("请选择要进行的操作："));
+        waitChooseOptList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        filePanel.add(waitChooseFileList);
+        filePanel.add(waitChooseOptList);
+
+
+        // 如果有重新选择目录的需求的话，点击“浏览”按钮即可
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                // 打开一个文件选择器
+                JFileChooser fc = new JFileChooser();
+                // 设置为只能选择目录
+                fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                // 打开文件选择对话框并保存返回的状态
+                int status = fc.showOpenDialog(null);
+
+                if (status == fc.APPROVE_OPTION) {
+                    // 如果选择成功，则修改本JFrame中的配置参数
+                    mTextField.setText(fc.getSelectedFile().toString());
+                    mRootDir = new File(fc.getSelectedFile().toString());
+                    mFileList = fc.getSelectedFile().list();
+
+                } else {
+                    // 否则维持原样，什么也不干
+                    System.out.println("cancel！");
+                }
+            }
+        });
+
         return filePanel;
+    }
+
+
+    /**
+     * 当调用此方法时，需要进行访问控制的根目录很可能已经发生了改变，
+     * 相应地，成员变量mFileList中的内容很有可能也已经发生了改变
+     * 因此需要调用此方法，来刷新UI界面中已经显示出的文件列表
+     */
+    private void invalidateUI() {
+        // TODO
     }
 
     /**
      * 配置并返回用户面板
+     *
      * @return
      */
-    private JPanel getUserPanel(){
+    private JPanel getUserPanel() {
         JPanel userPanel = new JPanel(new GridLayout(2, 1));
         userPanel.add(new Label("用户面板"));
         return userPanel;
     }
 
 
-    /**
-     * 配置并返回演示面板
-     * @return
-     */
-    private JPanel getShowPanel(){
-        JPanel showPanel = new JPanel(new GridLayout(2, 1));
-        showPanel.add(new Label("演示面板"));
-        return showPanel;
-    }
-
-
     // main method
     public static void main(String[] agrs) {
-        new MainFrame().setVisible(true);
+
+        // 先创建好主界面，但不显示
+        MainFrame mainFrame = new MainFrame();
+
+        // 首先打开一个文件选择器
+        JFileChooser fc = new JFileChooser();
+        // 只能选择目录
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        // 打开文件选择对话框并保存返回的状态
+        int status = fc.showOpenDialog(null);
+
+
+        if (status == fc.APPROVE_OPTION) {
+            // 如果选择成功，配置mainFrame中的初始参数
+            mainFrame.mTextField.setText(fc.getSelectedFile().toString());
+            mainFrame.mRootDir = new File(fc.getSelectedFile().toString());
+            // BUG
+            mainFrame.mFileList = fc.getSelectedFile().list();
+            mainFrame.setVisible(true);
+
+        } else {
+            // 异常退出(点击了取消按钮)，则直接退出
+            System.out.println("quit！");
+        }
     }
 }
